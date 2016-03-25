bba4473a4e4599bc74360d32fdb7465288083287
SteveC <stevechenweb@gmail.com>
2016-03-25 13:17:42 -0400

    Add new session log-in view. Comment out extra debug information from layout. Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,10 @@ ActiveRecord::Schema.define(version: 20160227005609) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "password"
+    t.string   "password_confirmation"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "password_digest"
   end
 
