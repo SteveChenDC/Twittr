15fcd39d9812323ab7ad8f3ef37c5218d9e3e4f2
SteveC <stevechenweb@gmail.com>
2016-05-29 00:39:05 -0400

    Fix root route.rb conflict

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'application#hello'
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
