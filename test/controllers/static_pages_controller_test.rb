00ffa1be0d74594579c9d7b4d2e22def189d16b7
SteveC <stevechenweb@gmail.com>
2016-05-29 00:37:45 -0400

    Make changes to static_pages_controller and views

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,20 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get :home
+    get :home # issuing browser request to the /home action
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
 end
