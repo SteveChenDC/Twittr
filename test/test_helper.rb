25efee9783c374f6b98d0fde74554cb04ff59b82
SteveC <stevechenweb@gmail.com>
2016-06-19 23:46:02 -0400

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,14 +19,15 @@ class ActiveSupport::TestCase
     password    = options[:password]    || 'password'
     remember_me = options[:remember_me] || '1'
     if integration_test?
-      post login_path, session: { email:     user.email,
+      post login_path, session: { email:    user.email,
                                  password:  password,
                                  remember_me: remember_me}
     else
       session[:user_id] = user.id
     end
   end
-    private
+
+  private
     # Returns true inside an integration test.
       def integration_test?
         defined?(post_via_redirect)
